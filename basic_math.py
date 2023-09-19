360c378137c0b926e921064a0f9506e403a9f605
Anastasia Friedenstein-Patino <anastasia.friedenstein-patino@stu.ocsb.ca>
2023-09-19 21:52:58 +0000

    Commit Rectangle Area and Perimeter

---
+++ basic_math.py
@@ -0,0 +1,6 @@
+print("14+21 = {}".format(14 + 21))
+print("33-17 = {}".format(33 - 17))
+print("12*3 = {}".format(12 * 3))
+print("23/2 = {}".format(23 / 2))
+print("2**3 = {}".format(2**3))
+print("2**0.5 = {}".format(2**0.5))
